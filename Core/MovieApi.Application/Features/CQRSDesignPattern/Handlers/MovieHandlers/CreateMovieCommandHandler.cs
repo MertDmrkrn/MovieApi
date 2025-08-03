4e0c28c9ec19690e397b1161e188b639dd170d3c
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-08-03 10:30:39 +0300

    Added CQRS and Mediator Design Patterns with API

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+	public class CreateMovieCommandHandler
+	{
+		private readonly MovieContext _context;
+
+		public CreateMovieCommandHandler(MovieContext context)
+		{
+			_context = context;
+		}
+
+		public async Task Handle(CreateMovieCommand command)
+		{
+			_context.Movies.Add(new Movie
+			{
+				MovieTitle = command.MovieTitle,
+				CoverImgUrl = command.CoverImgUrl,
+				Rating = command.Rating,
+				Description = command.Description,
+				Duration = command.Duration,
+				ReleaseDate = command.ReleaseDate,
+				CreatedYear = command.CreatedYear,
+				Status = command.Status
+			});
+			await _context.SaveChangesAsync();
+		}
+	}
+}
